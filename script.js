5ac2331becd0c2b36be0cdd8cc5284ee2f7c5da4
ashley11032001 <43544259+ashley11032001@users.noreply.github.com>
2019-03-20 10:04:28 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,24 @@
+//Spell #1
+
+$("img").click(function(){
+ $("img").hide();
+});
+
+
+
+//Spell #2
+
+$("      ").click(function(){
+ 
+});
+
+
+
+//Spell #3
+
+$("      ").click(function(){
+ 
+});
+
+
+
